bed0ef2ddb49cfb16f1c8a34e7503615f2e2cb08
muddassarH <muddassarhussain90@gmail.com>
2023-05-13 16:33:50 +0500

    j

---
+++ main.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+import pandas as pd
+app = Flask(__name__)
+
+df = pd.read_csv("dictionary.csv")
+
+variable = "HEllo There"
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+
+@app.route("/api/v1/<word>")
+def api_data(word):
+    definition = df.loc[df['word'] == word]['definition'].squeeze()
+    return {"word": word,
+            "definition": definition,
+            }
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5001)
